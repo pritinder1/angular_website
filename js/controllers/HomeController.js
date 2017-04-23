3e6b7b90009b2bb4df2a795e23353720a268f212
pritinder1 <Pritinder@alisons-iphone.resource.campus.njit.edu>
2017-04-23 09:44:23 -0400

    config movies for one item

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -110,7 +110,15 @@ app.controller('HomeController', ['$scope', function($scope) {
 	/* ADD FUNCTIONS FOR STEP 7 HERE */
 		
 	
-	
+	$scope.like = function(index){
+
+            $scope.movies[index]++;
+      }
+
+      $scope.dislikes = function(index){
+
+            $scope.movies[index]--;
+      }
 	
 	
 	
